f933901430c113d6ff90034b0b44eb90787e0ae5
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-16 14:44:53 -0300

    Added `invert` test suite.

--- test.js
+++ test.js
@@ -4,8 +4,16 @@ var truco = require('./');
 var test = require('tape');
 
 test('truco features', function(t) {
-  t.deepEqual(truco('Lorem Ipsum Dolor Sit', 5), 'Lorem…', 'truncate');
-  t.deepEqual(truco('Lorem Ipsum Dolor Sit', 5, '...'), 'Lorem...', 'custom ellipsis');
+  t.deepEqual(truco('Lorem Ipsum'), 'Lorem Ipsum', 'should not add ellipsis if limit was not specified');
+  t.deepEqual(truco('Lorem Ipsum Dolor Sit', 5), 'Lorem…', 'truncate on specified limit');
+  t.deepEqual(truco('Lorem Ipsum Dolor Sit', 5, {ellipsis: '...'}), 'Lorem...', 'custom ellipsis');
   t.deepEqual(truco('Lorem Ipsum', 150), 'Lorem Ipsum', 'do not append ellipsis if doesn\'t need to')
+  t.end();
+});
+
+test('invert', function(t) {
+  t.deepEqual(truco('Lorem Ipsum', 5, {moonwalk: true}), '…Ipsum', 'truncate on specified limit');
+  t.deepEqual(truco('Lorem Ipsum Dolor Sit', 3, {moonwalk: true, ellipsis: '...'}), '...Sit', 'custom ellipsis');
+  t.deepEqual(truco('Lorem Ipsum', 150, {moonwalk: true}), 'Lorem Ipsum', 'do not append ellipsis if doesn\'t need to');
   t.end();
 });
